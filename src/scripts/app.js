2822239b9ba5b0aedb783f6963d59a3b9a35b746
Bastien Gathoye <bastien.gathoye@student.heaj.be>
2023-03-09 02:27:14 -0800

    update animation, set background to green

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
 import { gsap } from "gsap";
 
 gsap.to(".box", { duration: 2, rotationY: 180, backgroundColor: "black", scale: 1.2})
-gsap.to(".circle", { duration: 2, scale: 2, backgroundColor: "red"})
+gsap.to(".circle", { duration: 2, scale: 2, backgroundColor: "green"})
